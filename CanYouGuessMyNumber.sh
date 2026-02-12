3b320f148af6b57f6f4f73b0d7d3c9c162291240
Yuval Khalifa <ykhalifa@akamai.com>
2026-02-12 14:43:00 +0200

    First version

---
+++ CanYouGuessMyNumber.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Wellcome to the CanYouGuessMyNumber Game!"
+echo "========================================="
+echo
+echo "I have a number in mind. Can you guess it?"
+echo ""
+echo ""
+
+MY_NUMBER=$( expr "$RANDOM" % 100 )
+guesses=1
+read -p "What is your guess? " guess
+while [[ "${guess}" != "${MY_NUMBER}" ]]; do
+  if [[ "${guess}" > "${MY_NUMBER}" ]]; then
+    echo "That is too big"
+  else
+    echo "That is too small"
+  fi
+  read -p "What is your guess? " guess
+  (( guesses += 1 ))
+done
+
+echo "You WON after ${guesses} guesses !!!!!!!!!!!!!!!!!!"
